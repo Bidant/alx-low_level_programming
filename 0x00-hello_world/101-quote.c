08b8ab3e2831699103aebd4e3439e686234f47e0
Osagwu Godson <godson.osagwu@gmail.com>
2022-09-08 19:25:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <uninstd.h>
-/**
- * main - main block
- * Return: 1
- */
-int main(void)
-{
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"s);
-
-	return (1);
+#include <unistd.h> 
+  
+ /** 
+  * main - Entry point  
+  * Description: prints Dora's quote
+  * Return: Always 1 (Success) 
+  */ 
+ int main(void) 
+{ 
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+		    
+        return (1); 
 }
