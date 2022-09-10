58ea09c117ddfd27909beffb042acc003597d0c7
Osagwu Godson <godson.osagwu@gmail.com>
2022-09-09 18:41:19 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,22 +2,21 @@
 
 /**
  *
- *  * main - program that prints the numbers from 01 to 89
+ * main - program that prints the numbers from 01 to 89
  *
- *   * Numbers must be separated by , followed by a space
+ * Numbers must be separated by , followed by a space
  *
- *    * Numbers should be printed in ascending order with 2 digits
+ * Numbers should be printed in ascending order with 2 digits
  *
- *     * You can only usethe putchar function
+ * You can only usethe putchar function
  *
- *      * Return: 0
+ * Return: 0
  *
- *       */
+ */
 
 int main(void)
 
 {
-
 	int n1 = 48;
 
 	int n2;
@@ -27,13 +26,11 @@ int main(void)
 	while (n1 <= 57)
 
 	{
-
 		n2 = n1 + 1;
 
 		while (n2 <= 57)
 
 		{
-
 			putchar(n1);
 
 			putchar(n2);
@@ -41,23 +38,18 @@ int main(void)
 			if (n1 != 56 || n2 != 57)
 
 			{
-
 				putchar(com);
 
 				putchar(32);
-
 			}
 
 			n2 += 1;
-
 		}
 
 		n1 += 1;
-
 	}
 
 	putchar('\n');
 
 	return (0);
-
 }
