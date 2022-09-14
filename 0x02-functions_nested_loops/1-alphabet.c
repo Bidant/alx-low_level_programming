ed0b64acdea36d980b9df78b76d4332664442bfe
Osagwu Godson <godson.osagwu@gmail.com>
2022-09-14 08:16:54 -0700

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints out alphabet in low case
+ *
+ *   *
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_alphabet(void)
+
+{
+
+		int i;
+
+
+
+			for (i = 'a'; i <= 'z'; i++)
+
+					{
+
+								_putchar(i);
+
+									}
+
+				/* new line */
+
+				_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * add - add 2 integers
+ *
+ *   * @a: int a
+ *
+ *    * @b: int b
+ *
+ *     * Return: int
+ *
+ *      */
+
+int add(int a, int b)
+
+{
+
+		return (a + b);
+
+}
