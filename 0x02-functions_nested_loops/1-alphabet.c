9192158459c031f4e491edfde57b6045f21b872d
Osagwu Godson <godson.osagwu@gmail.com>
2022-09-14 13:17:34 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
-* print_alphabet - prints out alphabet in low case
-* Return: void
-*/
-
+ * print_alphabet - prints out alphabet in low case
+ *
+ * Return: void
+ */
 void print_alphabet(void)
-
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
-
 	{
-
 		_putchar(i);
 
 	}
 	/* new line */
-
 	_putchar('\n');
 }
