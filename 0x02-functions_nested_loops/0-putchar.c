fe68f2e6e4594b8a09d42a311a8c9cfa569e4c8c
Osagwu Godson <godson.osagwu@gmail.com>
2022-09-14 12:53:33 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,39 +3,24 @@
 
 
 /**
- *
- *  * main - Print some letters
- *
- *   * Return: 0
- *
- *    */
-
-
+* main - Print some letters
+*
+* Return: 0
+*/
 
 int main(void)
 
 {
-
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 										_putchar('\n');
 
-
-
-											return (0);
+										return (0);
 
 }
